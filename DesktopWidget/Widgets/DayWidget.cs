19664f96902c3067aaf009da9dc3582d871e5b18
Martin Danielsen <kalroth@gmail.com>
2015-12-26 22:14:23 +0100

    feat: add day of week and week number

---
+++ DesktopWidget/Widgets/DayWidget.cs
@@ -0,0 +1,22 @@
+﻿using DesktopWidget.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopWidget.Widgets
+{
+    public class DayWidget : IWidgetValue
+    {
+        public string GetName()
+        {
+            return "Day";
+        }
+
+        public dynamic GetValue()
+        {
+            return DateTime.Now.DayOfWeek.ToString();
+        }
+    }
+}
---
+++ DesktopWidget/Widgets/WeekWidget.cs
@@ -0,0 +1,37 @@
+﻿using DesktopWidget.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopWidget.Widgets
+{
+    public class WeekWidget : IWidgetValue
+    {
+        public string GetName()
+        {
+            return "Week";
+        }
+
+        public dynamic GetValue()
+        {
+            // This presumes that weeks start with Monday.
+            // Week 1 is the 1st week of the year with a Thursday in it.
+            var time = DateTime.Now;
+
+            // Seriously cheat.  If its Monday, Tuesday or Wednesday, then it'll 
+            // be the same week# as whatever Thursday, Friday or Saturday are,
+            // and we always get those right
+            DayOfWeek day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(time);
+            if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday)
+            {
+                time = time.AddDays(3);
+            }
+
+            // Return the week of our adjusted day
+            return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+        }
+    }
+}
